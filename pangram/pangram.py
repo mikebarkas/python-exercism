92649594e6a0732e83ef890d8f2d6e11c33a37f2
Mike Barkas <mike@barkas.com>
2016-06-11 12:05:55 -0400

    Filter user input and check pangram against alphabet.

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -2,3 +2,11 @@
 def is_pangram(phrase):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+    # Filter user input.
+    string = phrase.lower()
+    string = string.replace('"', '').replace('.', '')
+
+    for letter in alphabet:
+        if not letter in string:
+           return False
+    return True
