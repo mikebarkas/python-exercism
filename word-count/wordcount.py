9df771e89b3f7634971cfeb2a0b4cf3a7578d6ea
Mike Barkas <mike@barkas.com>
2016-06-17 17:57:14 -0400

    Starting word list count.

--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -1,2 +1,6 @@
 # Wordcount: count the occurrences of each word in that phrase.
 def word_count(phrase):
+    word_list = phrase.lower().split()
+    wordfreq = [word_list.count(p) for p in word_list]
+
+    return dict(zip(word_list, wordfreq))
