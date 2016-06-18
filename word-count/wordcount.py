d6f868df5b042acc33ef2bad6d2e0d4013237e84
Mike Barkas <mike@barkas.com>
2016-06-18 16:08:27 -0400

    Change regex to allow unicode flag.

--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -1,8 +1,7 @@
 import re
 # Wordcount: count the occurrences of each word in that phrase.
 def word_count(phrase):
-    phrase = re.sub('[^a-zA-Z0-9\?\n]', ' ', phrase.lower())
+    phrase = re.sub(r'\W+|_', ' ', phrase.lower(), flags=re.UNICODE)
     word_list = phrase.split()
     wordfreq = [word_list.count(p) for p in word_list]
-
     return dict(zip(word_list, wordfreq))
