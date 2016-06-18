36dce3379e796281cd8098798d76dd0c44c50c5e
Mike Barkas <mike@barkas.com>
2016-06-18 15:47:27 -0400

    Adding regex to remove new lines.

--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -1,6 +1,8 @@
+import re
 # Wordcount: count the occurrences of each word in that phrase.
 def word_count(phrase):
-    word_list = phrase.lower().split()
+    phrase = re.sub('[^a-zA-Z0-9\?\n]', ' ', phrase.lower())
+    word_list = phrase.split()
     wordfreq = [word_list.count(p) for p in word_list]
 
     return dict(zip(word_list, wordfreq))
