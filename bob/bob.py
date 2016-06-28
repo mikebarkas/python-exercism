2a1dec1b9ad20eb95cded815fb2df295571872f5
Mike Barkas <mike@barkas.com>
2016-06-27 21:43:56 -0400

    Create hey function for Bob.

--- bob/bob.py
+++ bob/bob.py
@@ -3,6 +3,7 @@
 #
 
 
-def hey(what):
-
-    return
+def hey(prompt):
+    test = not prompt.strip(), prompt.isupper(), prompt.endswith('?'), prompt.endswith(' '), True
+    reply = 'Fine. Be that way!', 'Whoa, chill out!', 'Sure.', 'Sure.', 'Whatever.'
+    return reply[test.index(True)]
